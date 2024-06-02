70d0d39b3d8b32d6b336c632a86d7b9c7a4a5f67
Lipin Vyacheslav <Kompoman32@yandex.ru>
2024-06-02 21:28:35 +0500

    some fixes

--- rt_group_dance/CfgPatches.hpp
+++ rt_group_dance/CfgPatches.hpp
@@ -4,6 +4,6 @@ class CfgPatches
     class RT_Dance: RT_GROUP_CFG_PATCHES
     {
         name = "RT Compatibility - Dancing";
-        requiredAddons[] = {"RT_Main", "RT_Utils", "a3_anims_f"};
+        requiredAddons[] = {"RT_Main", "RT_Utils", "a3_anims_f", "A3_Anims_F_Config_Sdr"};
     };
 };
--- rt_group_settings/functions/preInit.sqf
+++ rt_group_settings/functions/preInit.sqf
@@ -1,4 +1,4 @@
 RT_SETTINGS_category = "RT Group Compatibility";
 
-#include "fun\settings.sqf";
 #include "support\settings.sqf";
+#include "fun\settings.sqf";
