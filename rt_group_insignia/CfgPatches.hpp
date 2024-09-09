4bb439250d10a2549e75ce72d0705e770d3781ca
Lipin Vyacheslav <Kompoman32@yandex.ru>
2024-09-10 03:29:40 +0500

    add insignia

---
+++ rt_group_insignia/CfgPatches.hpp
@@ -0,0 +1,15 @@
+class CfgPatches
+{
+    class RT_Insigina
+    {
+        name = "RT Compatibility - Insigina";
+        requiredAddons[] = {"RT_Main"};
+        
+        units[] = {};
+        weapons[] = {};
+
+        author = "Kompoman32";
+        requiredVersion = 1.60;
+        skipWhenMissingDependencies = 0;
+    };
+};
---
+++ rt_group_insignia/config.cpp
@@ -0,0 +1,26 @@
+#include "CfgPatches.hpp"
+
+class CfgUnitInsignia
+{
+    class RT_insignia_logo
+    {
+        displayName = "RT Logo";
+        author = "Voxtell";
+        texture = "rt\insignia\data\rt_logo_ca.paa";
+        textureVehicle = "";
+    };
+    class RT_insignia_group_logo
+    {
+        displayName = "RT Group Logo";
+        author = "Voxtell";
+        texture = "rt\insignia\data\rt_group_logo_ca.paa";
+        textureVehicle = "";
+    };
+    class RT_insignia_old_logo
+    {
+        displayName = "RT Old Logo";
+        author = "Voxtell";
+        texture = "rt\insignia\data\rt_logo_old_ca.paa";
+        textureVehicle = "";
+    };
+};
