c0d46bced2b17936fefa3260d39bf4a8209b0fce
Lipin Vyacheslav <Kompoman32@yandex.ru>
2024-09-10 03:28:09 +0500

    fixed some class names

--- rt_group_respawn/CfgEventHandlers.hpp
+++ rt_group_respawn/CfgEventHandlers.hpp
@@ -1,18 +1,18 @@
 
 class Extended_PreStart_EventHandlers {
-    class RT_Settings {
-        init = "call compileScript ['rt\settings\functions\preStart.sqf']";
+    class RT_Respawn {
+        init = "call compileScript ['rt\respawn\functions\preStart.sqf']";
     };
 };
 
 class Extended_PreInit_EventHandlers {
-    class RT_Settings {
-        init = "call compileScript ['rt\settings\functions\preInit.sqf']";
+    class RT_Respawn {
+        init = "call compileScript ['rt\respawn\functions\preInit.sqf']";
     };
 };
 
 class Extended_PostInit_EventHandlers {
-    class RT_Settings {
-        init = "call compileScript ['rt\settings\functions\postInit.sqf']";
+    class RT_Respawn {
+        init = "call compileScript ['rt\respawn\functions\postInit.sqf']";
     };
 };
