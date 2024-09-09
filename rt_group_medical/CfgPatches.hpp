376ff8641e93868fb9b0e73d0f5d34f803d74189
Lipin Vyacheslav <Kompoman32@yandex.ru>
2024-09-10 03:29:57 +0500

    add arrows to medical assistant

---
+++ rt_group_medical/CfgPatches.hpp
@@ -0,0 +1,15 @@
+class CfgPatches
+{
+    class RT_Medical
+    {
+        name = "RT Compatibility - Medical";
+        requiredAddons[] = {"RT_Main", "ASAAYU_ACE_MEDICAL_ASSISTANT"};
+        units[] = {};
+
+        weapons[] = {};
+
+        author = "Kompoman32";
+        requiredVersion = 1.60;
+        skipWhenMissingDependencies = 1;
+    };
+};
---
+++ rt_group_medical/config.cpp
@@ -0,0 +1,6 @@
+#include "CfgPatches.hpp"
+#include "CfgEventHandlers.hpp"
+
+class AMA_menu {
+	onLoad="[180025] spawn AMA_fnc_dialogFadeIn; [] spawn AMA_fnc_initMenu; [] spawn RT_Medical_Assitant_Init";
+}
