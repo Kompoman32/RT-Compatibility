54421d9f2d368f4b748141fdcaf0dd44240b537b
Lipin Slava <Kompoman32@yandex.ru>
2025-09-15 15:18:39 +0500

    refactor action таскания себя

---
+++ rt_group_units/CfgVehicles.hpp
@@ -0,0 +1,21 @@
+class CfgVehicles {
+    class Man;
+    class CAManBase: Man {
+        class ACE_SelfActions {
+            class RT_AllowCarryYourself {
+                displayName = "$STR_RT_UNITS_Allow_Carry";
+                condition = "(RT_SETTINGS_INITIALIZATION_units call CBA_settings_fnc_get) && !visibleMap && !(_this#0 getVariable [RT_UNITS_VAR_CAN_CARRY, false])";
+                statement = "_this#0 setVariable [RT_UNITS_VAR_CAN_CARRY, true, true]";
+                showDisabled = 0;
+                icon = "z\ace\addons\dragging\UI\icons\person_carry.paa";
+            };
+            class RT_DisallowCarryYourself {
+                displayName = "$STR_RT_UNITS_Disallow_Carry";
+                condition = "(RT_SETTINGS_INITIALIZATION_units call CBA_settings_fnc_get) && !visibleMap && (_this#0 getVariable [RT_UNITS_VAR_CAN_CARRY, false])";
+                statement = "_this#0 setVariable [RT_UNITS_VAR_CAN_CARRY, false, true]";
+                showDisabled = 0;
+                icon = "z\ace\addons\dragging\UI\icons\person_carry.paa";
+            };
+        };
+    };
+}
